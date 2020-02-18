65b98392f0cde6101db6b201137f22da02b76924
natashagils <56641651+natashagils@users.noreply.github.com>
2020-02-18 12:33:14 -0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,39 @@
+var questions = [
+  {
+      title: "Most people who ride the TTC every morning feel?",
+      choices: ["Nothing", "Angry", "Happy", "Sad"],
+      answer: "Nothing"
+  },
+
+  {
+      title: "Which of the following is true for the TTC?",
+      choices: ["It's always on time", "There are no delays", "It's clean", "It's all we have"],
+      answer: "It's all we have"
+  },
+
+  {
+      title: "What is the most common reason for a subway delay?",
+      choices: ["Alien attack", "Broken TTC GPS", "Drunk road drivers", "Signal problems"],
+      answer: "Signal problems"
+  },
+
+  {
+      title: "Which of the following is NOT a TTC Subway Station:",
+      choices: ["Eglinton", "Yonge", "Trump", "Spadina"],
+      answer: "Trump"
+  },
+
+  {
+      title: "What is the typical subway passengers' daily commute prayer?",
+      choices: ["Please let me be on time today", "Pray? I control my own destiny"],
+      answer: "Please let me be on time today"
+  },
+
+  {
+      title: "The TTC stands for:",
+      choices: ["This That Cow", "To Thedius Clyde", "Toronto Transit Commission", "The Tall Canine"],
+      answer: "Toronto Transit Commission"
+  },
+
+
+];
